c93b94d41b179bfeb55a4ee1ffb9570c97ffc748
st-angelo <angelostatescu@gmail.com>
2024-07-24 19:01:12 +0300

    init form

--- blocks/test-form/test-form.js
+++ blocks/test-form/test-form.js
@@ -1,4 +1,16 @@
-export default function init(el) {
-  console.log(el);
-  el.innerHTML = 'form';
+import { setLibs } from '../../scripts/utils.js';
+
+const miloLibs = setLibs('/libs');
+
+export default async function init(el) {
+  const { createTag } = await import(`${miloLibs}/utils/utils.js`);
+
+  const form = createTag('form', { class: 'test-form' });
+
+  const firstNameLabel = createTag('label', { class: 'test-form-label' });
+  const firstNameInput = createTag('input', { class: 'test-form-input' });
+
+  form.append(firstNameLabel, firstNameInput);
+
+  el.append(form);
 }
